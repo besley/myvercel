c20911ee1dbdb2af4c1658276de0e39edf81a0a0
William ligong <william.ligong@hotmail.com>
2025-11-10 12:54:04 +0800

    updated fiels

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ fontFamily: 'system-ui', padding: '2rem' }}>
-      <h1>Hello from Vercel + Next.js</h1>
-      <p>访问 <a href="/api/hello">/api/hello</a> 查看 API 响应。</p>
-    </main>
-  );
-}
